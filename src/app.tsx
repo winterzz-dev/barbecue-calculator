c9d42d2b0eb3ce468ed57d3032b514a2d46001bb
Max Zernyaev <winterzz@internet.ru>
2025-04-28 18:42:50 +0300

    feat: Добавить блок поддержки, ховера для кнопок и исправление заголовка

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,7 @@ import { PartyDuration } from "./modules/party-duration";
 export const App = () => (
 	<main class="flex flex-col gap-8 py-10 px-4 w-screen max-w-lg m-auto">
 		<h1 class="text-xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-3xl dark:text-white">
-			Калькулятор шашлыка 🍗
+			дайшашлык - калькулятор шашлыка 🍗
 		</h1>
 
 		<div class="flex flex-col gap-10">
@@ -15,5 +15,25 @@ export const App = () => (
 			<PartyDuration />
 			<Calculate />
 		</div>
+
+		<p className="text-gray-500 dark:text-gray-400">
+			Было полезно? Поддержи проект:
+		</p>
+		<ul className="text-gray-500 list-disc list-inside dark:text-gray-400">
+			<li>
+				<span>Поставь звезду проекту на </span>
+				<a
+					href="https://github.com/winterzz-dev/barbecue-calculator"
+					className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+				>
+					Github
+				</a>
+			</li>
+			<li>Поделись с друзьями</li>
+			<li>
+				Добавь приложение на главный экран - так ты сможешь пользоваться им даже
+				без сети
+			</li>
+		</ul>
 	</main>
 );
--- src/ui/toggle-button.tsx
+++ src/ui/toggle-button.tsx
@@ -7,7 +7,7 @@ interface IProps {
 export const ToggleButton = ({ children, isPressed, onClick }: IProps) => (
 	<div
 		on:click={onClick}
-		class={`flex items-center justify-center size-16 rounded-lg border-2 text-4xl cursor-pointer ${isPressed ? "bg-slate-300 border-gray-600 dark:bg-gray-500 dark:border-gray-400" : "bg-slate-50 border-gray-400 dark:bg-gray-700 dark:border-gray-600"}`}
+		class={`flex items-center justify-center size-16 rounded-lg border-2 text-4xl cursor-pointer hover:bg-slate-200 dark:hover:bg-gray-400 ${isPressed ? "bg-slate-300 border-gray-600 dark:bg-gray-500 dark:border-gray-400" : "bg-slate-50 border-gray-400 dark:bg-gray-700 dark:border-gray-600"}`}
 	>
 		{children}
 	</div>
